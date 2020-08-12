847fcd2f103b2fb222daf2c63b854f4360441840
Piyush Kumar <piyush.kumarmaloo@gmail.com>
2020-08-12 22:04:57 +0530

    Linked List probs added

---
+++ DS and Algorithms/Data Structures/2. LinkedList/search_element_in_linked_list.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int info;
+    struct node *next;
+};
+struct node *start = NULL;
+void create(int);
+//void search();
+void display();
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int i, val;
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &val);
+        create(val);
+    }
+    int key;
+    scanf("%d", &key);
+    search(key);
+    display();
+}
+void create(int val)
+{
+    if (start == NULL)
+    {
+        struct node *new_node;
+        new_node = (struct node *)malloc(sizeof(struct node));
+        new_node->info = val;
+        new_node->next = NULL;
+        start = new_node;
+    }
+    else
+    {
+        struct node *new_node, *temp;
+        new_node = (struct node *)malloc(sizeof(struct node));
+        temp = start;
+        new_node->info = val;
+        new_node->next = NULL;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = new_node;
+    }
+}
+void search(int key)
+{
+    struct node *temp;
+    temp = start;
+    while(temp->next!=NULL)
+    {
+        if(temp->info==key)
+        {
+            printf("Found");
+            break;
+        }      
+    }
+}
