03e27fdfafed6cae1f72130c19188728a5d71a1b
Piyush Kumar <piyush.kumarmaloo@gmail.com>
2020-11-16 10:59:10 +0530

    New Probs

---
+++ 1_GFG Must Do Coding Problems/Arrays/even_odd_part.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+void swap(int *, int *);
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    i = -1;
+    int j = 0;
+    while (j < n)
+    {
+        if (arr[j] % 2 == 0)
+        {
+            i++;
+            swap(&arr[j], &arr[i]);
+        }
+        j++;
+    }
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+}
+void swap(int *px, int *py) /* interchange *px and *py */
+{
+    int temp;
+    temp = *px;
+    *px = *py;
+    *py = temp;
+}
